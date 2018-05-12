d2eec22ba9e98616cec736390ab39d51738e5c46
Luís Fernando Nascimento <luis.nascimento@nexaas.com>
2018-05-11 21:31:15 -0300

    Removing TiltProcessor class from JqueryTemplate

--- lib/jquery-tmpl-rails/jquery_template.rb
+++ lib/jquery-tmpl-rails/jquery_template.rb
@@ -35,20 +35,4 @@ module JqueryTmplRails
     end
   end
 
-  class TiltProcessor
-
-    def initialize(klass)
-      @klass = klass
-    end
-
-    def call(input)
-      filename = input[:filename]
-      data     = input[:data]
-      context  = input[:environment].context_class.new(input)
-
-      data = @klass.new(filename) { data }.render(context, {})
-      context.metadata.merge(data: data.to_str)
-    end
-  end
-
 end
